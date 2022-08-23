edb0a443151185d191677945579b5438d69991e7
Fandado <a.pyatetskaya.nastya@gmail.com>
2022-08-23 22:36:45 +0300

    init:start a project

---
+++ scripts/index.js
@@ -0,0 +1,511 @@
+import difficulties from '../data/difficulties.js';
+ 
+
+const blueCardsAssets = {
+    blue1: './assets/MythicCards/blue/blue1.png',
+    blue2: './assets/MythicCards/blue/blue2.png',
+    blue3: './assets/MythicCards/blue/blue3.png',
+    blue4: './assets/MythicCards/blue/blue4.png',
+    blue5: './assets/MythicCards/blue/blue5.png',
+    blue6: './assets/MythicCards/blue/blue6.png',
+    blue7: './assets/MythicCards/blue/blue7.png',
+    blue8: './assets/MythicCards/blue/blue8.png',
+    blue9: './assets/MythicCards/blue/blue9.png',
+    blue10: './assets/MythicCards/blue/blue10.png',
+    blue11: './assets/MythicCards/blue/blue11.png',
+    blue12: './assets/MythicCards/blue/blue12.png',
+  }
+
+const blueCardsData = [
+{
+    id: 'blue1',
+    cardFace: blueCardsAssets.blue1,
+    difficulty: 'hard',
+    color:'blue'
+},
+{
+    id: 'blue2',
+    cardFace: blueCardsAssets.blue2,
+    difficulty: 'hard',
+    color:'blue'
+},
+{
+    id: 'blue3',
+    cardFace: blueCardsAssets.blue3,
+    difficulty: 'easy',
+    color:'blue'
+},
+{
+    id: 'blue4',
+    cardFace: blueCardsAssets.blue4,
+    difficulty: 'easy',
+    color:'blue'
+},
+{
+    id: 'blue5',
+    cardFace: blueCardsAssets.blue5,
+    difficulty: 'easy',
+    color:'blue'
+},
+{
+    id: 'blue6',
+    cardFace: blueCardsAssets.blue6,
+    difficulty: 'hard',
+    color:'blue'
+},
+{
+    id: 'blue7',
+    cardFace: blueCardsAssets.blue7,
+    difficulty: 'normal',
+    color:'blue'
+},
+{
+    id: 'blue8',
+    cardFace: blueCardsAssets.blue8,
+    difficulty: 'hard',
+    color:'blue'
+},
+{
+    id: 'blue9',
+    cardFace: blueCardsAssets.blue9,
+    difficulty: 'normal',
+    color:'blue'
+},
+{
+    id: 'blue10',
+    cardFace: blueCardsAssets.blue10,
+    difficulty: 'easy',
+    color:'blue'
+},
+{
+    id: 'blue11',
+    cardFace: blueCardsAssets.blue11,
+    difficulty: 'normal',
+    color:'blue'
+},
+{
+    id: 'blue12',
+    cardFace: blueCardsAssets.blue12,
+    difficulty: 'normal',
+    color:'blue'
+},
+]
+
+ 
+
+const brownCardsAssets = {
+    brown1: './assets/MythicCards/brown/brown1.png',
+    brown2: './assets/MythicCards/brown/brown2.png',
+    brown3: './assets/MythicCards/brown/brown3.png',
+    brown4: './assets/MythicCards/brown/brown4.png',
+    brown5: './assets/MythicCards/brown/brown5.png',
+    brown6: './assets/MythicCards/brown/brown6.png',
+    brown7: './assets/MythicCards/brown/brown7.png',
+    brown8: './assets/MythicCards/brown/brown8.png',
+    brown9: './assets/MythicCards/brown/brown9.png',
+    brown10: './assets/MythicCards/brown/brown10.png',
+    brown11: './assets/MythicCards/brown/brown11.png',
+    brown12: './assets/MythicCards/brown/brown12.png',
+    brown13: './assets/MythicCards/brown/brown13.png',
+    brown14: './assets/MythicCards/brown/brown14.png',
+    brown15: './assets/MythicCards/brown/brown15.png',
+    brown16: './assets/MythicCards/brown/brown16.png',
+    brown17: './assets/MythicCards/brown/brown17.png',
+    brown18: './assets/MythicCards/brown/brown18.png',
+    brown19: './assets/MythicCards/brown/brown19.png',
+    brown20: './assets/MythicCards/brown/brown20.png',
+    brown21: './assets/MythicCards/brown/brown21.png',
+  }
+
+const brownCardsData = [
+    {
+      id: 'brown1',
+      cardFace: brownCardsAssets.brown1,
+      difficulty: 'normal',
+      color:'brown'
+    },
+    {
+      id: 'brown2',
+      cardFace: brownCardsAssets.brown2,
+      difficulty: 'normal',
+      color:'brown'
+    },
+    {
+      id: 'brown3',
+      cardFace: brownCardsAssets.brown3,
+      difficulty: 'normal',
+      color:'brown'
+    },
+    {
+      id: 'brown4',
+      cardFace: brownCardsAssets.brown4,
+      difficulty: 'normal',
+      color:'brown'
+    },
+    {
+      id: 'brown5',
+      cardFace: brownCardsAssets.brown5,
+      difficulty: 'normal',
+      color:'brown'
+    },
+    {
+      id: 'brown6',
+      cardFace: brownCardsAssets.brown6,
+      difficulty: 'hard',
+      color:'brown'
+    },
+    {
+      id: 'brown7',
+      cardFace: brownCardsAssets.brown7,
+      difficulty: 'hard',
+      color:'brown'
+    },
+    {
+      id: 'brown8',
+      cardFace: brownCardsAssets.brown8,
+      difficulty: 'hard',
+      color:'brown'
+    },
+    {
+      id: 'brown9',
+      cardFace: brownCardsAssets.brown9,
+      difficulty: 'hard',
+      color:'brown'
+    },
+    {
+      id: 'brown10',
+      cardFace: brownCardsAssets.brown10,
+      difficulty: 'hard',
+      color:'brown'
+    },
+    {
+      id: 'brown11',
+      cardFace: brownCardsAssets.brown11,
+      difficulty: 'easy',
+      color:'brown'
+    },
+    {
+      id: 'brown12',
+      cardFace: brownCardsAssets.brown12,
+      difficulty: 'easy',
+      color:'brown'
+    },
+    {
+      id: 'brown13',
+      cardFace: brownCardsAssets.brown13,
+      difficulty: 'easy',
+      color:'brown'
+    },
+    {
+      id: 'brown14',
+      cardFace: brownCardsAssets.brown14,
+      difficulty: 'easy',
+      color:'brown'
+    },
+    {
+      id: 'brown15',
+      cardFace: brownCardsAssets.brown15,
+      difficulty: 'normal',
+      color:'brown'
+    },
+    {
+      id: 'brown16',
+      cardFace: brownCardsAssets.brown16,
+      difficulty: 'normal',
+      color:'brown'
+    },
+    {
+      id: 'brown17',
+      cardFace: brownCardsAssets.brown17,
+      difficulty: 'normal',
+      color:'brown'
+    },
+    {
+      id: 'brown18',
+      cardFace: brownCardsAssets.brown18,
+      difficulty: 'normal',
+      color:'brown'
+    },
+    {
+      id: 'brown19',
+      cardFace: brownCardsAssets.brown19,
+      difficulty: 'normal',
+      color:'brown'
+    },
+    {
+      id: 'brown20',
+      cardFace: brownCardsAssets.brown20,
+      difficulty: 'normal',
+      color:'brown'
+    },
+    {
+      id: 'brown21',
+      cardFace: brownCardsAssets.brown21,
+      difficulty: 'easy',
+      color:'brown'
+    },
+  ]
+
+ 
+
+const greenCardsAssets = {
+    green1: './assets/MythicCards/green/green1.png',
+    green2: './assets/MythicCards/green/green2.png',
+    green3: './assets/MythicCards/green/green3.png',
+    green4: './assets/MythicCards/green/green4.png',
+    green5: './assets/MythicCards/green/green5.png',
+    green6: './assets/MythicCards/green/green6.png',
+    green7: './assets/MythicCards/green/green7.png',
+    green8: './assets/MythicCards/green/green8.png',
+    green9: './assets/MythicCards/green/green9.png',
+    green10: './assets/MythicCards/green/green10.png',
+    green11: './assets/MythicCards/green/green11.png',
+    green12: './assets/MythicCards/green/green12.png',
+    green13: './assets/MythicCards/green/green13.png',
+    green14: './assets/MythicCards/green/green14.png',
+    green15: './assets/MythicCards/green/green15.png',
+    green16: './assets/MythicCards/green/green16.png',
+    green17: './assets/MythicCards/green/green17.png',
+    green18: './assets/MythicCards/green/green18.png',
+    green19: './assets/MythicCards/green/green19.png',
+    green20: './assets/MythicCards/green/green20.png',
+    green21: './assets/MythicCards/green/green21.png',
+  };
+
+const greenCardsData = [
+    {
+      id: 'green1',
+      cardFace: greenCardsAssets.green1,
+      difficulty: 'easy',
+      color:'green'
+    },
+    {
+      id: 'green2',
+      cardFace: greenCardsAssets.green2,
+      difficulty: 'hard',
+      color:'green'
+    },
+    {
+      id: 'green3',
+      cardFace: greenCardsAssets.green3,
+      difficulty: 'hard',
+      color:'green'
+    },
+    {
+      id: 'green4',
+      cardFace: greenCardsAssets.green4,
+      difficulty: 'hard',
+      color:'green'
+    },
+    {
+      id: 'green5',
+      cardFace: greenCardsAssets.green5,
+      difficulty: 'hard',
+      color:'green'
+    },
+    {
+      id: 'green6',
+      cardFace: greenCardsAssets.green6,
+      difficulty: 'hard',
+      color:'green'
+    },
+    {
+      id: 'green7',
+      cardFace: greenCardsAssets.green7,
+      difficulty: 'normal',
+      color:'green'
+    },
+    {
+      id: 'green8',
+      cardFace: greenCardsAssets.green8,
+      difficulty: 'normal',
+      color:'green'
+    },
+    {
+      id: 'green9',
+      cardFace: greenCardsAssets.green9,
+      difficulty: 'normal',
+      color:'green'
+    },
+    {
+      id: 'green10',
+      cardFace: greenCardsAssets.green10,
+      difficulty: 'normal',
+      color:'green'
+    },
+    {
+      id: 'green11',
+      cardFace: greenCardsAssets.green11,
+      difficulty: 'normal',
+      color:'green'
+    },
+    {
+      id: 'green12',
+      cardFace: greenCardsAssets.green12,
+      difficulty: 'easy',
+      color:'green'
+    },
+    {
+      id: 'green13',
+      cardFace: greenCardsAssets.green13,
+      difficulty: 'normal',
+      color:'green'
+    },
+    {
+      id: 'green14',
+      cardFace: greenCardsAssets.green14,
+      difficulty: 'normal',
+      color:'green'
+    },
+    {
+      id: 'green15',
+      cardFace: greenCardsAssets.green15,
+      difficulty: 'normal',
+      color:'green'
+    },
+    {
+      id: 'green16',
+      cardFace: greenCardsAssets.green16,
+      difficulty: 'easy',
+      color:'green'
+    },
+    {
+      id: 'green17',
+      cardFace: greenCardsAssets.green17,
+      difficulty: 'easy',
+      color:'green'
+    },
+    {
+      id: 'green18',
+      cardFace: greenCardsAssets.green18,
+      difficulty: 'easy',
+      color:'green'
+    },
+  ]
+
+const ancientsData = [
+    {
+      id: 'azathoth',
+      name: 'azathoth',
+    //   cardFace: Ancients.azathoth,
+      firstStage: {
+        greenCards: 1,
+        blueCards: 1,
+        brownCards: 2,
+      },
+      secondStage: {
+        greenCards: 2,
+        blueCards: 1,
+        brownCards: 3,
+      },
+      thirdStage: {
+        greenCards: 2,
+        blueCards: 0,
+        brownCards: 4,
+      },
+    },
+    {
+      id: 'cthulhu',
+      name: 'cthulhu',
+    //   cardFace: Ancients.cthulhu,
+      firstStage: {
+        greenCards: 0,
+        blueCards: 2,
+        brownCards: 2,
+      },
+      secondStage: {
+        greenCards: 1,
+        blueCards: 0,
+        brownCards: 3,
+      },
+      thirdStage: {
+        greenCards: 3,
+        blueCards: 0,
+        brownCards: 4,
+      },
+    },
+    {
+      id: 'iogSothoth',
+      name: 'iogSothoth',
+    //   cardFace: Ancients.iogSothoth,
+      firstStage: {
+        greenCards: 0,
+        blueCards: 1,
+        brownCards: 2,
+      },
+      secondStage: {
+        greenCards: 2,
+        blueCards: 1,
+        brownCards: 3,
+      },
+      thirdStage: {
+        greenCards: 3,
+        blueCards: 0,
+        brownCards: 4,
+      },
+    },
+    {
+      id: 'shubNiggurath',
+      name: 'shubNiggurath',
+    //   cardFace: Ancients.shubNiggurath,
+      firstStage: {
+        greenCards: 1,
+        blueCards: 1,
+        brownCards: 2,
+      },
+      secondStage: {
+        greenCards: 3,
+        blueCards: 1,
+        brownCards: 2,
+      },
+      thirdStage: {
+        greenCards: 2,
+        blueCards: 0,
+        brownCards: 4,
+      },
+    },
+  ]
+console.log(ancientsData);
+
+const lastCard = document.querySelector(".last-card");
+
+///генерировать рандом для карт, этап 1  (генерирует массив)
+const greenRandom1 = _.shuffle(_.range(1,18)).slice(0, ancientsData[0].firstStage.greenCards)
+console.log(greenRandom1)
+
+const blueRandom1 = _.shuffle(_.range(1,12)).slice(0, ancientsData[0].firstStage.blueCards)
+console.log(blueRandom1)
+
+const brownRandom1 = _.shuffle(_.range(1,21)).slice(0, ancientsData[0].firstStage.brownCards)
+console.log(brownRandom1)
+
+///генерировать рандом для карт, этап 2  (генерирует массив)
+const greenRandom2 = _.shuffle(_.range(1,18)).slice(0, ancientsData[0].secondStage.greenCards)
+console.log(greenRandom2)
+
+const blueRandom2 = _.shuffle(_.range(1,12)).slice(0, ancientsData[0].secondStage.blueCards)
+console.log(blueRandom2)
+
+const brownRandom2 = _.shuffle(_.range(1,21)).slice(0, ancientsData[0].secondStage.brownCards)
+console.log(brownRandom2)
+
+///генерировать рандом для карт, этап 3  (генерирует массив)
+const greenRandom3 = _.shuffle(_.range(1,18)).slice(0, ancientsData[0].thirdStage.greenCards)
+console.log(greenRandom3)
+
+const blueRandom3 = _.shuffle(_.range(1,12)).slice(0, ancientsData[0].thirdStage.blueCards)
+console.log(blueRandom3)
+
+const brownRandom3 = _.shuffle(_.range(1,21)).slice(0, ancientsData[0].thirdStage.brownCards)
+console.log(brownRandom3)
+
+///вывод на экран карты
+function setCard(greenRandom1) {
+    const img = new Image();
+    img.src = "./assets/MythicCards/green/green18.png";
+    img.onload = () => {
+        lastCard.style.backgroundImage = "url('./assets/MythicCards/green/green18.png')";
+    };
+}
+
+setCard(greenRandom1);
